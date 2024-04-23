789d541f41bea9166ba184a47bdd9beae5a8e3c7
mizutismask <severinek@gmail.com>
2024-04-23 16:11:18 +0200

    fix: cube card location db field was not long enough for new users

--- dbmodel.sql
+++ dbmodel.sql
@@ -46,7 +46,7 @@ CREATE TABLE IF NOT EXISTS `animalCube` (
     `card_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
     `card_type` varchar(16) NOT NULL,
     `card_type_arg` int(11) NOT NULL,   
-    `card_location` varchar(16) NOT NULL,
+    `card_location` varchar(20) NOT NULL,
     `card_location_arg` int(11) NOT NULL,
     PRIMARY KEY (`card_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
