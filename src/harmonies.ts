86c36264e8f35211466a37785ad945fc75a88bbd
mizutismask <severinek@gmail.com>
2024-05-27 13:23:51 +0200

    feat: shows active zones

--- src/harmonies.ts
+++ src/harmonies.ts
@@ -447,6 +447,7 @@ class Harmonies implements HarmoniesGame {
 			if (args.canChooseSpirit) {
 				this.river.setSelectionMode('none')
 				this.playerTables[this.getPlayerId()].setSpiritSelectionMode('single')
+				$(`spirits-${this.getPlayerId()}`).classList.add("active-zone")
 			} else {
 				if (args.canPlaceAnimalCube && Object.keys(args.placeAnimalCubeArgs).length == 1) {
 					const cardId = parseInt(Object.keys(args.placeAnimalCubeArgs)[0])
@@ -464,6 +465,7 @@ class Harmonies implements HarmoniesGame {
 				if (args.canTakeAnimalCard) {
 					this.river.setSelectionMode('single')
 					this.river.setSelectableCards(this.river.getCards())
+					$("river").classList.add("active-zone")
 				} else {
 					this.river.setSelectionMode('none')
 				}
@@ -485,6 +487,7 @@ class Harmonies implements HarmoniesGame {
 	public onLeavingState(stateName: string) {
 		console.log('Leaving state: ' + stateName)
 		removeClass('selected-element')
+		removeClass('active-zone')
 
 		switch (stateName) {
 			/* Example:
