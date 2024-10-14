56b01a29b1df4d35a81f2660b155c703d543af12
mizutismask <severinek@gmail.com>
2024-10-14 10:22:44 +0200

    fix: INFORMATION_SCHEMA is shared between all games, so get constraints from each and every game. This is not compatible with getUniqueValueFromDB. Should have used "select database()", but now UC_CellLevel has been removed manually

--- harmonies.game.php
+++ harmonies.game.php
@@ -322,16 +322,6 @@ class Harmonies extends Table {
 
     function upgradeTableDb($from_version) {
 
-        $constraintExists =  "SELECT CONSTRAINT_NAME
-                            FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS
-                            WHERE TABLE_NAME = 'coloredToken'
-                            AND CONSTRAINT_TYPE = 'UNIQUE'
-                            AND CONSTRAINT_NAME = 'UC_CellLevel';";
-        $result = self::getUniqueValueFromDB($constraintExists);
-        if (!empty($result)) {
-            $this->customApplyDbUpgrade($from_version, "all", "ALTER TABLE `DBPREFIX_coloredToken` DROP INDEX `UC_CellLevel`;");
-        }
-
         $resetTokensPosition = "UPDATE `DBPREFIX_coloredToken` t1
                         JOIN (
                             SELECT t2.card_id,
